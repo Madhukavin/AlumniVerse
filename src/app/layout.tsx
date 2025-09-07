75de8af4bbb55f88598e8a760d858f137be69726
Madhukavin M <madhukavinmadhukavin7880@gmail.com>
2025-09-07 08:17:23 +0000

    Also apply this icon to the title of the actual website in the browser w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🏫</text></svg>"
+        />
       </head>
       <body className="font-sans antialiased">
         <AuthProvider>
